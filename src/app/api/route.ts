0e7689a1cf2906745b25368788eb9a884a4f4a73
kimsohyeon <aotoyae@gmail.com>
2024-03-15 00:05:42 +0900

    test: request 테스트

---
+++ src/app/api/route.ts
@@ -0,0 +1,19 @@
+export async function GET(request: Request) {
+  console.log('GET');
+}
+
+export async function POST(request: Request) {
+  console.log('POST');
+}
+
+export async function PUT(request: Request) {
+  console.log('PUT');
+}
+
+export async function DELETE(request: Request) {
+  console.log('DELETE');
+}
+
+export async function PATCH(request: Request) {
+  console.log('PATCH');
+}
