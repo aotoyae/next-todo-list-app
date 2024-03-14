6bb4aa76d7e03438e893af2f8aa6d6b33eb52a6c
kimsohyeon <aotoyae@gmail.com>
2024-03-15 00:36:59 +0900

    Fix: GET, POST json 으로 요청

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,31 @@
 export async function GET(request: Request) {
-  console.log('GET');
+  const response = await fetch('http://localhost:4000/todos');
+  const todos = await response.json();
+
+  if (!todos) {
+    return new Response('Todo is not found', {
+      status: 404,
+    });
+  }
+
+  return Response.json({
+    todos,
+  });
 }
 
 export async function POST(request: Request) {
-  console.log('POST');
+  const { title, contents } = await request.json();
+  const response = await fetch('http://localhost:4000/todos', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ title, contents, isDone: false }),
+  });
+
+  const todo = await response.json();
+
+  return Response.json({ todo });
 }
 
 export async function PUT(request: Request) {
