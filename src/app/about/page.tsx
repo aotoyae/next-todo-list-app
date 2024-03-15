fbc836d5915d269a3485fa2f071d05105454fcde
kimsohyeon <aotoyae@gmail.com>
2024-03-15 09:53:04 +0900

    Add: companyInfo GET

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,21 @@
-function page() {
-  return <div>about</div>;
-}
+import Image from 'next/image';
 
-export default page;
+const AboutPage = async () => {
+  const response = await fetch('http://localhost:4000/companyInfo', {
+    cache: 'force-cache',
+  });
+  const { name, desctiption, image } = await response.json();
+
+  if (!name) return <h1>loading</h1>;
+
+  return (
+    <div>
+      <h1>about</h1>
+      <Image src={image} alt="company image" width={400} height={400} />
+      <h2>{name}</h2>
+      <p>{desctiption}</p>
+    </div>
+  );
+};
+
+export default AboutPage;
