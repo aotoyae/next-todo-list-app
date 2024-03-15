1582d95ab29c80a84f4bcfb9cb5a1e7be50577c9
kimsohyeon <aotoyae@gmail.com>
2024-03-15 11:48:17 +0900

    Add: todo 현황 10초마다 가져오기

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,5 +1,22 @@
-const ReportPage = () => {
-  return <div>ReportPage</div>;
+import { Todo } from '@/types/todo';
+
+const ReportPage = async () => {
+  const response = await fetch('http://localhost:4000/todos', {
+    next: { revalidate: 10 },
+  });
+  const todos = await response.json();
+
+  const ongoingTodos = todos.filter((todo: Todo) => todo.isDone === false);
+  const finishTodos = todos.filter((todo: Todo) => todo.isDone === true);
+
+  return (
+    <div>
+      <h1>todo-list report</h1>
+      <h2>
+        {todos.length}개의 todo 중에서 {finishTodos.length}개를 완료했어요!
+      </h2>
+    </div>
+  );
 };
 
 export default ReportPage;
