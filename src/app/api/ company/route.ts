7974b17399085ead8535bc3968e142f472169cf7
kimsohyeon <aotoyae@gmail.com>
2024-03-15 14:25:17 +0900

    Refacor: try catch 문으로 수정

---
+++ src/app/api/ company/route.ts
@@ -0,0 +1,12 @@
+export async function GET(request: Request) {
+  try {
+    const response = await fetch('http://localhost:4000/company');
+    const company = await response.json();
+
+    return Response.json({
+      company,
+    });
+  } catch (error) {
+    throw new Error('GET Company Error');
+  }
+}
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,41 +1,57 @@
 export async function GET(request: Request) {
-  const response = await fetch('http://localhost:4000/todos');
-  const todos = await response.json();
+  try {
+    const response = await fetch('http://localhost:4000/todos');
+    const todos = await response.json();
 
-  if (!todos) {
-    return new Response('Todo is not found', {
-      status: 404,
-    });
+    if (!todos) {
+      return new Response('Todo is not found', {
+        status: 404,
+      });
+    }
+
+    return Response.json({ todos });
+  } catch (error) {
+    throw new Error('GET Todos Error');
   }
-
-  return Response.json({
-    todos,
-  });
 }
 
 export async function POST(request: Request) {
-  const { title, contents } = await request.json();
-  const response = await fetch('http://localhost:4000/todos', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-    },
-    body: JSON.stringify({ title, contents, isDone: false }),
-  });
+  try {
+    const { title, contents } = await request.json();
+    const response = await fetch('http://localhost:4000/todos', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ title, contents, isDone: false }),
+    });
 
-  const todo = await response.json();
+    const todo = await response.json();
 
-  return Response.json({ todo });
-}
-
-export async function PUT(request: Request) {
-  console.log('PUT');
-}
-
-export async function DELETE(request: Request) {
-  console.log('DELETE');
+    return Response.json({ todo });
+  } catch (error) {
+    throw new Error('POST Todos Error');
+  }
 }
 
 export async function PATCH(request: Request) {
-  console.log('PATCH');
+  try {
+    console.log('PATCH');
+  } catch (error) {
+    throw new Error('PATCH Todos Error');
+  }
+}
+
+export async function DELETE(request: Request) {
+  try {
+    const id = await request.json();
+    const response = await fetch(`http://localhost:4000/todos/${id}`, {
+      method: 'DELETE',
+    });
+
+    const todos = await response.json();
+    return Response.json({ todos });
+  } catch (error) {
+    throw new Error('DELETE Todos Error');
+  }
 }
